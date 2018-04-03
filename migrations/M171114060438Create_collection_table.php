fca3279763ae50472633bd8a818e0d78ecaa7ef7
xutongle <xutongle@gmail.com>
2018-04-03 11:12:10 +0800

    init

---
+++ migrations/M171114060438Create_collection_table.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace yuncms\collection\migrations;
+
+use yii\db\Migration;
+
+class M171114060438Create_collection_table extends Migration
+{
+
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        /**
+         * 用户收藏表
+         */
+        $this->createTable('{{%collections}}', [
+            'id' => $this->primaryKey()->unsigned()->comment('ID'),
+            'user_id' => $this->integer()->unsigned()->notNull()->comment('User Id'),
+            'model_id' => $this->bigInteger()->notNull()->comment('Model Id'),
+            'model_class' => $this->string()->notNull()->comment('Model Class'),
+            'subject' => $this->string()->comment('Subject'),
+            'created_at' => $this->integer()->unsigned()->notNull()->comment('Created At'),
+            'updated_at' => $this->integer()->unsigned()->notNull()->comment('Updated At'),
+        ], $tableOptions);
+
+        $this->createIndex('collections_index', '{{%collections}}', ['user_id', 'model_id', 'model_class'], true);
+        $this->addForeignKey('{{%collections_fk_1}}', '{{%collections}}', 'user_id', '{{%user}}', 'id', 'CASCADE', 'RESTRICT');
+
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%collections}}');
+    }
+
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "M171114060438Create_collection_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ src/models/Collection.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\collection\models;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+use yuncms\db\ActiveRecord;
+use yuncms\notifications\contracts\NotificationInterface;
+use yuncms\notifications\NotificationTrait;
+use yuncms\user\models\User;
+
+/**
+ * This is the model class for table "collections".
+ *
+ * @property integer $user_id
+ * @property integer $model_id
+ * @property string $model_class
+ * @property string $subject
+ * @property integer $created_at
+ * @property integer $updated_at
+ * @property ActiveRecord $source
+ *
+ * @property User $user
+ */
+class Collection extends ActiveRecord implements NotificationInterface
+{
+    use NotificationTrait;
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%collections}}';
+    }
+
+    /**
+     * 是否收藏
+     * @param string $model
+     * @param integer $modelId
+     * @param integer $user_id
+     * @return bool
+     */
+    public static function isCollected($model, $modelId, $user_id = null)
+    {
+        return static::find()->where([
+            'user_id' => $user_id ? $user_id : Yii::$app->user->getId(),
+            'model_class' => $model,
+            'model_id' => $modelId
+        ])->exists();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::class,
+            [
+                'class' => BlameableBehavior::class,
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'user_id',
+                ],
+            ]];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['model_id'], 'required'],
+            [['subject'], 'filter', 'filter' => 'trim'],
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQueryInterface
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'user_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSource()
+    {
+        return $this->hasOne($this->model_class, ['id' => 'model_id']);
+    }
+
+    /**
+     * @param bool $insert
+     * @param array $changedAttributes
+     */
+    public function afterSave($insert, $changedAttributes)
+    {
+        if ($insert) {
+            $this->source->updateCountersAsync(['collections' => 1]);
+            try {
+                Yii::$app->notification->send($this->source->user, $this);
+            } catch (InvalidConfigException $e) {
+            }
+        }
+        parent::afterSave($insert, $changedAttributes);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function afterDelete()
+    {
+        $this->source->updateCountersAsync(['collections' => -1]);
+        parent::afterDelete();
+    }
+}
